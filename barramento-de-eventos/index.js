5da823e2aaf655edec657fe080eb0e1b4fa37907
Carneiro06 <133177811+Carneiro06@users.noreply.github.com>
2024-11-09 22:05:12 -0300

    feat(t2): implanta com Docker

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -1,21 +1,39 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 //para enviar eventos para os demais microsserviços
 const axios = require('axios');
 
+const { PORT } = process.env
 const app = express();
 app.use(bodyParser.json());
 const eventos = []
 
-app.post('/eventos', (req, res) => {
+app.post('/eventos', async (req, res) => {
     const evento = req.body;
     eventos.push(evento);
-
-    axios.post('http://localhost:4000/eventos', evento);
-    axios.post('http://localhost:5000/eventos', evento);
-    axios.post('http://localhost:6000/eventos', evento);
-    axios.post('http://localhost:7000/eventos', evento);
-    axios.post('http://localhost:8000/eventos', evento);
+    try {
+        await axios.post('http://logs-clusterip-service:2000/eventos', evento)
+    }
+    catch (err) { }
+    try {
+        await axios.post('http://lembretes-clusterip-service:4000/eventos', evento)
+    }
+    catch (err) { }
+    try {
+        await axios.post('http://observacoes-clusterip-service:5000/eventos', evento)
+    }
+    catch (err) {
+    }
+    try {
+        await axios.post('http://consulta-clusterip-service:6000/eventos', evento)
+    }
+    catch (err) {
+    }
+    try {
+        await axios.post('http://classificacao-clusterip-service:7000/eventos', evento)
+    }
+    catch (err) { }
     res.status(200).send({ msg: "ok" });
 });
 
@@ -24,6 +42,4 @@ app.get('/eventos', (req, res) => {
 })
 
 
-app.listen(10000, () => {
-    console.log('Barramento de eventos. Porta 10000.')
-})
+app.listen(PORT, () => console.log(`Barramento. Porta ${PORT}.`))
